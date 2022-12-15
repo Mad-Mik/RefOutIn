c4dd2ea284ccab4b9484df977a8ba987354a7053
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-15 22:38:34 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RefOutIn
+{
+    public class Person
+    {
+        static void Main(string[] args)
+        {
+            // ref/out/in
+            int m = 4;
+            Console.WriteLine(m);
+            Sum(ref m, 5, out int k);
+            Console.WriteLine(m);
+            Console.WriteLine(k);
+
+        }
+        static int Sum(ref int m, int n, out int k)
+        {
+            k = 0;
+            m++;
+            int S = m + n + k;
+            return S;
+        }
+    }
+}
